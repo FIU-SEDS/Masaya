9da923f6dfc676dd216dfb3106403ea329af6668
marcio <marciogarcial412@gmail.com>
2026-03-03 14:49:37 -0500

    Created Servo Valves Custom Lib

---
+++ lib/ServoValves/servoValve.cpp
@@ -0,0 +1,18 @@
+#include "servoValve.h"
+
+servoValve::servoValve(int servoPin) {
+    _servoPin = servoPin;
+}
+
+void servoValve::begin() {
+    _servo.attach(_servoPin);
+    close();
+}
+
+void servoValve::close() {
+    _servo.write(0);
+}
+
+void servoValve::open() {
+    _servo.write(180);
+}
---
+++ lib/ServoValves/servoValve.h
@@ -0,0 +1,19 @@
+#ifndef SERVO_VALVE_H
+#define SERVO_VALVE_H
+
+#include <Arduino.h>
+#include <Servo.h>
+
+class servoValve {
+  private:
+    int _servoPin;
+    Servo _servo;
+
+  public:
+    servoValve(int servoPin);
+    void begin();
+    void close();
+    void open();
+};
+
+#endif
