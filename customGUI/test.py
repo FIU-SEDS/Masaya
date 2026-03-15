ea548376eb0e961329fcc3bba3f16e9abbfeeb40
Marcio Garcia <155027035+marci0garcia@users.noreply.github.com>
2026-03-14 22:55:16 -0400

    Added Start/Stop buttons, implmented check box

--- customGUI/test.py
+++ customGUI/test.py
@@ -1,5 +1,5 @@
 import sys, os, socket
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QCheckBox
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
 
@@ -43,88 +43,127 @@ class DiagramWindow(QMainWindow):
         label.setMaximumSize(1500, 900)
         self.setCentralWidget(label)
 
-        
+        self.name = QLabel("Cold Flow Test", label)
+        self.name.move(20,20)
+        self.name.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+
+        self.SEV04OX = QCheckBox("SEV-04-OX",self)
+        self.SEV04OX.move(20,200)
+        self.SEV04OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.SEV04OX.adjustSize()
+
+        self.START = QPushButton("START", self)
+        self.START.move(20,740)
+        self.START.resize(300,50)
+        self.START.setStyleSheet("""
+            QPushButton {
+                color: white; 
+                font-size: 20px; 
+                font-weight: bold; 
+                background-color: green;
+            }
+            QPushButton:pressed {
+                background-color: darkgreen;
+            }
+        """)
+        self.START.clicked.connect(self.START_Test)
+
+        self.STOP = QPushButton("STOP", self)
+        self.STOP.move(20,800)
+        self.STOP.resize(300,80)
+        self.STOP.setStyleSheet("""
+            QPushButton {
+                color: white; 
+                font-size: 20px; 
+                font-weight: bold; 
+                background-color: red;
+            }
+            QPushButton:pressed {
+                background-color: darkred;
+            }
+        """)
+        self.STOP.clicked.connect(self.STOP_Test)
+
+
         # Load Cells 
 
         self.LC01F = QLabel("12345", label)
         self.LC01F.move(664,470)
-        self.LC01F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.LC01F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.LC01F.adjustSize()
 
         self.LC02OX = QLabel("12345", label)
         self.LC02OX.move(664,409)
-        self.LC02OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.LC02OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.LC02OX.adjustSize()
-
         
         # Thermal Couplers
 
         self.TC01F = QLabel("12345", label)
         self.TC01F.move(230,558)
-        self.TC01F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC01F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.TC01F.adjustSize()
 
         self.TC02OX = QLabel("12345", label)
         self.TC02OX.move(230,324)
-        self.TC02OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC02OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.TC02OX.adjustSize()
 
         self.TC03OX = QLabel("12345", label)
         self.TC03OX.move(934,282)
-        self.TC03OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC03OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.TC03OX.adjustSize()
 
         self.TC02F = QLabel("12345", label)
         self.TC02F.move(930,596)
-        self.TC02F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC02F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.TC02F.adjustSize()
-    
 
         # PT Sensors
 
         self.PT01F = QLabel("12345", label)
         self.PT01F.move(446,558)
-        self.PT01F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT01F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT01F.adjustSize()
 
         self.PT02F = QLabel("12345", label)
         self.PT02F.move(659,709)
-        self.PT02F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT02F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT02F.adjustSize()
 
         self.PT03F = QLabel("12345", label)
         self.PT03F.move(885,709)
-        self.PT03F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT03F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT03F.adjustSize()
 
         self.PT04F = QLabel("12345", label)
         self.PT04F.move(1183,596)
-        self.PT04F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT04F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT04F.adjustSize()
 
         self.PT05E = QLabel("12345", label)
         self.PT05E.move(1359,509)
-        self.PT05E.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT05E.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT05E.adjustSize()
 
         self.PT06OX = QLabel("12345", label)
         self.PT06OX.move(1183,282)
-        self.PT06OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT06OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT06OX.adjustSize()
 
         self.PT07OX = QLabel("12345", label)
         self.PT07OX.move(885,157)
-        self.PT07OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT07OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT07OX.adjustSize()
 
         self.PT08OX = QLabel("12345", label)
         self.PT08OX.move(657,158)
-        self.PT08OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT08OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT08OX.adjustSize()
 
         self.PT09OX = QLabel("12345", label)
         self.PT09OX.move(446,324)
-        self.PT09OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT09OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
         self.PT09OX.adjustSize()
 
 
@@ -137,6 +176,14 @@ class DiagramWindow(QMainWindow):
         """Called automatically when new UDP data arrives"""
         self.TC02OX.setText(value)
         self.TC02OX.adjustSize()
+
+    def START_Test(self):
+        self.PT01F.setText("Start")
+
+    def STOP_Test(self):
+        self.PT01F.setText("STOP")
+
+    
 
 
 if __name__ == "__main__":
