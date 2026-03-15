3d10ba2be99ee6da6873480f8a673d9698526c82
Marcio Garcia <155027035+marci0garcia@users.noreply.github.com>
2026-03-14 22:15:31 -0400

    Added text Labels for all sensors

--- customGUI/test.py
+++ customGUI/test.py
@@ -15,41 +15,128 @@ class UDPListener(QThread):
 
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((self.ip, self.port))
-        print(f"Listening on {self.ip}:{self.port}")
+        
+        try:
+            sock.bind((self.ip, self.port))
+            print(f"Listening on {self.ip}:{self.port}")
+        except OSError as e:
+            print(f"Error: Could not bind to {self.ip}:{self.port}. {e}")
+            return 
 
         while True:
             data, addr = sock.recvfrom(1024)
             text = data.decode("utf-8", errors="ignore").strip()
-            self.data_received.emit(text)  
-
+            self.data_received.emit(text)
 
 class DiagramWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Piping Diagram")
-        self.resize(1280, 720)
+        self.resize(1500, 900)
+        self.setMaximumSize(1500, 900)
 
         label = QLabel()
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        label.setPixmap(QPixmap(os.path.join(script_dir, "assets", "PID_2.png")))
+        label.setPixmap(QPixmap(os.path.join(script_dir, "assets", "PID_Masaya.png")))
         label.setScaledContents(True)
-        label.setMinimumSize(1280, 720)
+        label.setMinimumSize(1500, 900)
+        label.setMaximumSize(1500, 900)
         self.setCentralWidget(label)
 
-        self.pressure = QLabel("--", label)
-        self.pressure.move(194, 274)
-        self.pressure.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
-        self.pressure.adjustSize()
+        
+        # Load Cells 
+
+        self.LC01F = QLabel("12345", label)
+        self.LC01F.move(664,470)
+        self.LC01F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.LC01F.adjustSize()
+
+        self.LC02OX = QLabel("12345", label)
+        self.LC02OX.move(664,409)
+        self.LC02OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.LC02OX.adjustSize()
+
+        
+        # Thermal Couplers
+
+        self.TC01F = QLabel("12345", label)
+        self.TC01F.move(230,558)
+        self.TC01F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC01F.adjustSize()
+
+        self.TC02OX = QLabel("12345", label)
+        self.TC02OX.move(230,324)
+        self.TC02OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC02OX.adjustSize()
+
+        self.TC03OX = QLabel("12345", label)
+        self.TC03OX.move(934,282)
+        self.TC03OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC03OX.adjustSize()
+
+        self.TC02F = QLabel("12345", label)
+        self.TC02F.move(930,596)
+        self.TC02F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.TC02F.adjustSize()
+    
+
+        # PT Sensors
+
+        self.PT01F = QLabel("12345", label)
+        self.PT01F.move(446,558)
+        self.PT01F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT01F.adjustSize()
+
+        self.PT02F = QLabel("12345", label)
+        self.PT02F.move(659,709)
+        self.PT02F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT02F.adjustSize()
+
+        self.PT03F = QLabel("12345", label)
+        self.PT03F.move(885,709)
+        self.PT03F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT03F.adjustSize()
+
+        self.PT04F = QLabel("12345", label)
+        self.PT04F.move(1183,596)
+        self.PT04F.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT04F.adjustSize()
+
+        self.PT05E = QLabel("12345", label)
+        self.PT05E.move(1359,509)
+        self.PT05E.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT05E.adjustSize()
+
+        self.PT06OX = QLabel("12345", label)
+        self.PT06OX.move(1183,282)
+        self.PT06OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT06OX.adjustSize()
+
+        self.PT07OX = QLabel("12345", label)
+        self.PT07OX.move(885,157)
+        self.PT07OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT07OX.adjustSize()
+
+        self.PT08OX = QLabel("12345", label)
+        self.PT08OX.move(657,158)
+        self.PT08OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT08OX.adjustSize()
+
+        self.PT09OX = QLabel("12345", label)
+        self.PT09OX.move(446,324)
+        self.PT09OX.setStyleSheet("color: red; font-size: 20px; font-weight: bold;")
+        self.PT09OX.adjustSize()
+
+
 
         self.udp_thread = UDPListener(ip="192.168.1.100", port=5005)
-        self.udp_thread.data_received.connect(self.update_pressure)  
+        self.udp_thread.data_received.connect(self.update_SENSORS)  
         self.udp_thread.start()
 
-    def update_pressure(self, value):
+    def update_SENSORS(self, value):
         """Called automatically when new UDP data arrives"""
-        self.pressure.setText(value)
-        self.pressure.adjustSize()
+        self.TC02OX.setText(value)
+        self.TC02OX.adjustSize()
 
 
 if __name__ == "__main__":
