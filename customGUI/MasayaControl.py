4d0773f5ec1d76651567816c9fb8394726d58396
Marcio Garcia <155027035+marci0garcia@users.noreply.github.com>
2026-03-15 14:27:44 -0400

    Changed pixel ratio's to better fit laptop screen

--- customGUI/MasayaControl.py
+++ customGUI/MasayaControl.py
@@ -32,28 +32,30 @@ class DiagramWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Masaya Control")
-        self.resize(1500, 900)
-        self.setMaximumSize(1500, 900)
+        self.resize(1333, 800)
+        self.setFixedSize(1333, 800)
 
         label = QLabel()
         script_dir = os.path.dirname(os.path.abspath(__file__))
         label.setPixmap(QPixmap(os.path.join(script_dir, "assets", "PID_Masaya.png")))
         label.setScaledContents(True)
-        label.setMinimumSize(1500, 900)
-        label.setMaximumSize(1500, 900)
+        label.setMinimumSize(1333, 800)
+        label.setMaximumSize(1333, 800)
         self.setCentralWidget(label)
 
+        label_style = "color: white; font-size: 18px; font-weight: bold;"
+
         self.name = QLabel("Cold Flow Test", label)
-        self.name.move(20,20)
-        self.name.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.name.move(18, 18)
+        self.name.setStyleSheet(label_style)
 
 
         self.step1 = QCheckBox("Step1",self)
-        self.step1.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.step1.setStyleSheet(label_style)
         self.step1.adjustSize()
 
         self.step2 = QCheckBox("Step2",self)
-        self.step2.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.step2.setStyleSheet(label_style)
         self.step2.adjustSize()
 
 
@@ -62,25 +64,18 @@ class DiagramWindow(QMainWindow):
         self.checkBoxes.addWidget(self.step2)
 
         self.checkBoxContainer = QWidget(self)
-
         self.checkBoxContainer.setLayout(self.checkBoxes)
-
-        self.checkBoxContainer.move(20, 200)
-
+        self.checkBoxContainer.move(18, 178) 
         self.checkBoxContainer.adjustSize()
         
 
-
-
-
-
-        self.START = QPushButton("START", self)
-        self.START.move(20,740)
-        self.START.resize(300,50)
+        self.START = QPushButton("GO", self)
+        self.START.move(18, 658) 
+        self.START.resize(267, 44) 
         self.START.setStyleSheet("""
             QPushButton {
                 color: white; 
-                font-size: 20px; 
+                font-size: 18px; 
                 font-weight: bold; 
                 background-color: green;
             }
@@ -88,15 +83,15 @@ class DiagramWindow(QMainWindow):
                 background-color: darkgreen;
             }
         """)
-        self.START.clicked.connect(self.START_Test)
+        self.START.clicked.connect(self.GO)
 
         self.STOP = QPushButton("STOP", self)
-        self.STOP.move(20,800)
-        self.STOP.resize(300,80)
+        self.STOP.move(18, 711) 
+        self.STOP.resize(267, 71) 
         self.STOP.setStyleSheet("""
             QPushButton {
                 color: white; 
-                font-size: 20px; 
+                font-size: 18px; 
                 font-weight: bold; 
                 background-color: red;
             }
@@ -108,86 +103,82 @@ class DiagramWindow(QMainWindow):
 
 
         # Load Cells 
-
         self.LC01F = QLabel("12345", label)
-        self.LC01F.move(664,470)
-        self.LC01F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.LC01F.move(590, 418) 
+        self.LC01F.setStyleSheet(label_style)
         self.LC01F.adjustSize()
 
         self.LC02OX = QLabel("12345", label)
-        self.LC02OX.move(664,409)
-        self.LC02OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.LC02OX.move(590, 364)
+        self.LC02OX.setStyleSheet(label_style)
         self.LC02OX.adjustSize()
         
         # Thermal Couplers
-
         self.TC01F = QLabel("12345", label)
-        self.TC01F.move(230,558)
-        self.TC01F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.TC01F.move(204, 496) 
+        self.TC01F.setStyleSheet(label_style)
         self.TC01F.adjustSize()
 
         self.TC02OX = QLabel("12345", label)
-        self.TC02OX.move(230,324)
-        self.TC02OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.TC02OX.move(204, 288) 
+        self.TC02OX.setStyleSheet(label_style)
         self.TC02OX.adjustSize()
 
         self.TC03OX = QLabel("12345", label)
-        self.TC03OX.move(934,282)
-        self.TC03OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.TC03OX.move(830, 251)
+        self.TC03OX.setStyleSheet(label_style)
         self.TC03OX.adjustSize()
 
         self.TC02F = QLabel("12345", label)
-        self.TC02F.move(930,596)
-        self.TC02F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.TC02F.move(827, 530)
+        self.TC02F.setStyleSheet(label_style)
         self.TC02F.adjustSize()
 
         # PT Sensors
-
         self.PT01F = QLabel("12345", label)
-        self.PT01F.move(446,558)
-        self.PT01F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT01F.move(396, 496)
+        self.PT01F.setStyleSheet(label_style)
         self.PT01F.adjustSize()
 
         self.PT02F = QLabel("12345", label)
-        self.PT02F.move(659,709)
-        self.PT02F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT02F.move(586, 630)
+        self.PT02F.setStyleSheet(label_style)
         self.PT02F.adjustSize()
 
         self.PT03F = QLabel("12345", label)
-        self.PT03F.move(885,709)
-        self.PT03F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT03F.move(787, 630)
+        self.PT03F.setStyleSheet(label_style)
         self.PT03F.adjustSize()
 
         self.PT04F = QLabel("12345", label)
-        self.PT04F.move(1183,596)
-        self.PT04F.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT04F.move(1052, 530)
+        self.PT04F.setStyleSheet(label_style)
         self.PT04F.adjustSize()
 
         self.PT05E = QLabel("12345", label)
-        self.PT05E.move(1359,509)
-        self.PT05E.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT05E.move(1208, 452)
+        self.PT05E.setStyleSheet(label_style)
         self.PT05E.adjustSize()
 
         self.PT06OX = QLabel("12345", label)
-        self.PT06OX.move(1183,282)
-        self.PT06OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT06OX.move(1052, 251)
+        self.PT06OX.setStyleSheet(label_style)
         self.PT06OX.adjustSize()
 
         self.PT07OX = QLabel("12345", label)
-        self.PT07OX.move(885,157)
-        self.PT07OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT07OX.move(787, 140)
+        self.PT07OX.setStyleSheet(label_style)
         self.PT07OX.adjustSize()
 
         self.PT08OX = QLabel("12345", label)
-        self.PT08OX.move(657,158)
-        self.PT08OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT08OX.move(584, 140)
+        self.PT08OX.setStyleSheet(label_style)
         self.PT08OX.adjustSize()
 
         self.PT09OX = QLabel("12345", label)
-        self.PT09OX.move(446,324)
-        self.PT09OX.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+        self.PT09OX.move(396, 288)
+        self.PT09OX.setStyleSheet(label_style)
         self.PT09OX.adjustSize()
-
 
 
         self.udp_thread = UDPListener(ip="192.168.1.100", port=5005)
@@ -195,27 +186,28 @@ class DiagramWindow(QMainWindow):
         self.udp_thread.start()
 
     def update_SENSORS(self, value):
-        """Called automatically when new UDP data arrives"""
         self.TC02OX.setText(value)
         self.TC02OX.adjustSize()
 
-    def START_Test(self):
-        if(self.step1.isChecked()):
-            self.PT01F.setText("Start")
+    def GO(self):
+        if(self.step1.isChecked() and not self.step2.isChecked()):
+            self.popUp = QMessageBox(self)
+            self.popUp.setWindowTitle("Warning")
+            self.popUp.setText("Second Step isn't complete\nDo the Following: \nMake sure Manual Valves are open\n")
+            self.popUp.exec()
+        elif(self.step1.isChecked() and self.step2.isChecked()):
+            self.popUp = QMessageBox(self)
+            self.popUp.setWindowTitle("Warning")
+            self.popUp.setText("Third isn't complete\nDo the Following: \nMake sure Manual Valves are open\n")
+            self.popUp.exec()
         else:
             self.popUp = QMessageBox(self)
             self.popUp.setWindowTitle("Warning")
-            self.popUp.setText("MAKE SURE EVERYTHING IS GOOD")
+            self.popUp.setText("First Step isn't complete\nDo the Following: \nMake sure Manual Valves are open\n")
             self.popUp.exec()
-
-        
 
     def STOP_Test(self):
         self.PT01F.setText("STOP")
-
-    
-
-    
 
 
 if __name__ == "__main__":
